2b1c30e0915fecfb6361db257e5a7e3eeef3ffb6
hadiaimtiaz <imtiazhadia75@gmail.com>
2025-02-22 15:58:07 +0500

    Create week3.js

---
+++ week3.js
@@ -0,0 +1,33 @@
+let objArr = [
+    {
+        name:'john',
+        age:12,
+        gender:'male'
+    },
+    {
+        name:'hadia',
+        age:24,
+        gender:'female'
+    },
+    {
+        name:'fatima',
+        age:21,
+        gender:'female'
+    }
+
+];
+console.log("first object in array using the [] notation:")
+console.log(objArr[0]);
+
+const obj={name:'hadia',age:21}
+module.exports=obj;
+const obj=require('./week3.js')
+console.log(obj);
+
+mypromise.then((val)=>
+  {
+    console.log(val)
+    }).catch ((err)=>
+  {
+    console.log(err)
+  })
