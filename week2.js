9634ef8ef06536f30f56e464d1dd964ba302d353
hadiaimtiaz <imtiazhadia75@gmail.com>
2025-02-22 15:44:23 +0500

    Update week2.js

--- week2.js
+++ week2.js
@@ -5,3 +5,14 @@ function triple(n)
 }
 let new_arr=arr.map(triple)
 console.log(new_arr);
+const names=["john","hadia","neha"]
+names.filter((name)=>
+    {
+        return name!="neha"
+    })
+function product(a,b){
+    return a*b;
+}
+const arr=[1,2,3,5];
+var product_of_arr=arr.reduce(product)
+console.log(product_of_arr)
