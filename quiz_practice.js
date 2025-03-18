06deb3295211f609343dedeb45a45efc61c27afe
hadiaimtiaz <imtiazhadia75@gmail.com>
2025-03-18 19:16:44 +0500

    Create quiz_practice.js

---
+++ quiz_practice.js
@@ -0,0 +1,104 @@
+/  var num1= 11;
+//  var num2= 20;
+// // const print= ()=>{
+// //     var num2= 20;
+// //     console.log(num1);
+// //     console.log(num2);
+// // }
+
+// // if (num1 == 10){
+// //     let num3= 30;
+// //     console.log(num3);
+
+// // }
+
+// (num1== 10? console.log(num1): console.log(num2));
+
+// // var num4; 
+// // console.log(num1);
+// // console.log(num4);
+
+// // print();
+// // function doAdd(num1, num2) {
+// //     if(arguments.length === 1){
+// //         console.log(num1 + 10);
+// // }
+// //     else if(arguments.length === 2){
+// //         console.log(num1 + num2);
+// //     }
+// // }
+
+// // doAdd(10);
+// // doAdd(5, 7);
+// // const prd = (number) => {
+// //     console.log(number * 2);
+// // }
+// // const numbers = [1, 2, 3, 4]; 
+// // numbers.forEach(prd);
+
+// const numbers = [1, 2, 3, 4]; 
+// numbers.forEach(number => {
+//   console.log(number * 2);
+// });
+
+// var a= 11;
+// let b= 'hadia';
+// const c= true;
+// var d= 2.9;
+// var e= null;
+// var f;
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// console.log(typeof(f));
+
+//console.log(isNaN(a));
+
+// arr = [{name: 'ayesha', age: 19}, {name: 'hadiya', age: 20}, {name: 'sana', age: 21}];
+// export arr;
+
+// function My(a) {
+//     console.log(a);
+
+// }
+
+// function cal(a, b, callBack){
+//     let sum = a + b;
+//     callBack(sum);
+// }
+
+// cal(2, 3, My);
+
+// setTimeout(myFunc, 3000);
+// Myfun2();
+// function myFunc(){
+//     console.log('Hello');
+// }
+
+// function Myfun2(){
+//     console.log('Hello2');
+//}
+
+// let myPromise = new Promise((resolve, reject) => {
+//     let a = 1 + 2;
+//     if(a == 2){
+//         resolve('Success');
+//     }
+//     else{
+//         reject('Failed');
+//     }
+// });
+
+// myPromise.then((message) => {
+//     console.log(message); // This will log 'Success'
+// }).catch((message) => {
+//     console.log(message); // This will log 'Failed' if the promise is rejected
+// });
+
+// async function myFunction() {
+//     console.log('Hello') ;
+// }
+
+// await myFunction(); // This will return 'Hello'
