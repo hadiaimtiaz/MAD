ab360e1c46af43f421ddb22891f2355d87d890ad
hadiaimtiaz <imtiazhadia75@gmail.com>
2025-03-18 19:12:36 +0500

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,14 @@
+export default function counter() {
+    const [count, setCount] = useState(0);
+
+    return(
+    <View style={styles.container}>
+          <Text style={styles.counterText}>Counter: {count}</Text>
+          <View style={styles.buttonContainer}>
+            <Button title="Increase" onPress={() => setCount(count + 1)} />
+            <Button title="Decrease" onPress={() => setCount(count - 1)} />
+            <Button title="Reset" onPress={() => setCount(0)} />
+          </View>
+        </View>
+    )
+}
